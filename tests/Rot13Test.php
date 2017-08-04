5c120e7d7c7c308b73edeac61f393cbf68d6c6e8
mario <mario@studyportals.eu>
2017-08-04 16:00:12 +0200

    Initial Commit

---
+++ tests/Rot13Test.php
@@ -0,0 +1,17 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+function Rot13( $input ){
+    return str_rot13 ( $input );
+}
+
+final class Rot13Test extends TestCase
+{
+    public function testStringToRot13()
+    {
+        $this->assertEquals(
+            Rot13('Hello World'),
+            'Uryyb Jbeyq'
+        );
+    }
+}
